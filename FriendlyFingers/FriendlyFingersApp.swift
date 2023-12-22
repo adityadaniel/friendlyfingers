c589e81700ac6ba539e6a36f5d4922ad49c233eb
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2023-12-22 09:11:27 +0700

    Initial Commit

---
+++ FriendlyFingers/FriendlyFingersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyFingersApp.swift
+//  FriendlyFingers
+//
+//  Created by Daniel Aditya Istyana on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyFingersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
